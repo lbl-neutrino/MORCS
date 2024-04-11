aa0025f69ccd0be54357ea647f785014ba0e4875
Matt Kramer <mjkramer@users.noreply.github.com>
2024-04-11 08:22:39 -0700

    Update lrs.py to use Flask API

--- src/morcs/subsystems/lrs.py
+++ src/morcs/subsystems/lrs.py
@@ -1,48 +1,23 @@
 #!/usr/bin/env python3
 
-from enum import Enum
-import socket
-import uuid
+import requests
 
-from .base import DaqController
-
-
-class LrsMsgType(Enum):
-    CONFIG_V4 = 0xAE2E6D03
-    CMD_V4 = 0xAE2E6D04
-
-
-def encode(msgtype: LrsMsgType, cmd: str, *args: str) -> bytes:
-    payload = ' '.join([cmd, *args]).encode() + b'\r\n'
-    magic = msgtype.value.to_bytes(4, 'little')
-    length = len(payload).to_bytes(4, 'little')
-    return magic + length + payload
-
+from ..controller import DaqController
 
 class LrsController(DaqController):
-    def __init__(self, config: dict):
-        super().__init__(config)
-
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((config['lrs'].get('remote_addr', 'localhost'),
-                           config['lrs'].get('remote_port', 33334)))
-
-    def send(self, msgtype: LrsMsgType, cmd: str, *args: str):
-        msg = encode(msgtype, cmd, *args)
-        self.sock.sendall(msg)
+    def get_url(self, function):
+        host = self.config['lrs']['host']
+        return f'http://{host}/api/{function}/'
 
     def start_run(self):
-        self.uuid = str(uuid.uuid4())
-
-        self.send(LrsMsgType.CMD_V4,
-                  'start',
-                  self.config['lrs'].get('global_index', '0'),
-                  self.config['lrs'].get('run', '0'),
-                  self.uuid)
+        params = {
+            'run': self.db.next_run(),
+        }
+        if stream := self.config['global'].get('data_stream'):
+            params['data_stream'] = stream
+        url = self.get_url('start_data_run')
+        requests.post(url, json=params)
 
     def stop_run(self):
-        self.send(LrsMsgType.CMD_V4,
-                  'stop',
-                  self.config['lrs'].get('global_index', '0'),
-                  self.config['lrs'].get('run', '0'),
-                  self.uuid)
+        url = self.get_url('stop_data_run')
+        requests.get(url)
